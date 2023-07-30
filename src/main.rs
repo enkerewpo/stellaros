ab655833a8cce8aca4fbee431a4f66ed078cd4bb
wheatfox <enkerewpo@hotmail.com>
2023-07-30 23:17:22 +0800

    basic printing

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,68 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+mod vga_buffer;
+mod serial;
 
 use core::panic::PanicInfo;
 
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+  serial_println!("Running {} tests", tests.len());
+  for test in tests {
+    test();
+  }
+  exit_qemu(QemuExitCode::Success);
+}
+
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+  println!("{}", _info);
   loop {}
 }
 
-static HELLO: &[u8] = b"Hello World from wheatfox!";
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+  serial_println!("[failed]\n");
+  serial_println!("Error: {}\n", info);
+  exit_qemu(QemuExitCode::Failed);
+  loop {}
+}
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-  let vga_buffer = 0xb8000 as *mut u8;
-  for(i, &byte) in HELLO.iter().enumerate() {
-    unsafe {
-      *vga_buffer.offset(i as isize * 2) = byte;
-      *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-    }
-  }
+  let hello = "StellarOS v0.1.0, wheatfox, 2023";
+  println!("{}", hello);
+  #[cfg(test)]
+  test_main();
   loop {}
+}
+
+// Test code
+#[test_case]
+fn trivial_assertion() {
+  serial_print!("trivial assertion... ");
+  assert_eq!(0, 1);
+  serial_println!("[ok]");
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+  Success = 0x10,
+  Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+  use x86_64::instructions::port::Port;
+
+  unsafe {
+    let mut port = Port::new(0xf4);
+    port.write(exit_code as u32);
+  }
 }
